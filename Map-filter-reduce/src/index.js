da1bd57c19aab133b86a9393f8fb4672780004ac
mdnobin96670 <farhanahmednobin@gmail.com>
2024-05-26 01:24:44 +0600

    map filter reduce

---
+++ Map-filter-reduce/src/index.js
@@ -0,0 +1,39 @@
+var numbers = [3, 48, 2, 56, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+// const newNum = numbers.map((num) => num * num);
+// console.log(newNum);
+
+//Filter - Create a new array by keeping the items that return true.
+
+// const newNum = numbers.filter((num) => num > 20);
+
+// console.log(newNum);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+// const newNum = numbers.reduce((accumulator, currentNumer) => {
+//   console.log(accumulator);
+//   return accumulator + currentNumer;
+// });
+
+// console.log(newNum);
+
+//Find - find the first item that matches from an array.
+
+// const newNum = numbers.find((num) => num > 10);
+
+// console.log(newNum);
+
+//FindIndex - find the index of the first item that matches.
+
+// const newNum = numbers.findIndex((num) => num > 48);
+
+// console.log(newNum);
+
+// import emojipedia from "./emojipedia";
+
+// const emneing = emojipedia.map((details) => details.meaning.substring(0, 101));
+
+// console.log(emneing);
